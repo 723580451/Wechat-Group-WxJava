6e75ca258b47da6fc89b302b32a91527a125b010
Binary Wang <binarywang@gmail.com>
2018-08-25 18:24:42 +0800

    #309 企业微信新增常量类WxCpConsts，存放包含事件类型在内的一些常量

--- weixin-java-common/src/main/java/me/chanjar/weixin/common/api/WxConsts.java
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/api/WxConsts.java
@@ -151,14 +151,32 @@ public class WxConsts {
     public static final String CLICK = "CLICK";
     public static final String VIEW = "VIEW";
     public static final String MASS_SEND_JOB_FINISH = "MASSSENDJOBFINISH";
+    /**
+     * 扫码推事件的事件推送
+     */
     public static final String SCANCODE_PUSH = "scancode_push";
+    /**
+     * 扫码推事件且弹出“消息接收中”提示框的事件推送.
+     */
     public static final String SCANCODE_WAITMSG = "scancode_waitmsg";
+    /**
+     * 弹出系统拍照发图的事件推送.
+     */
     public static final String PIC_SYSPHOTO = "pic_sysphoto";
+    /**
+     * 弹出拍照或者相册发图的事件推送.
+     */
     public static final String PIC_PHOTO_OR_ALBUM = "pic_photo_or_album";
+    /**
+     * 弹出微信相册发图器的事件推送.
+     */
     public static final String PIC_WEIXIN = "pic_weixin";
+    /**
+     * 弹出地理位置选择器的事件推送.
+     */
     public static final String LOCATION_SELECT = "location_select";
+
     public static final String TEMPLATE_SEND_JOB_FINISH = "TEMPLATESENDJOBFINISH";
-    public static final String ENTER_AGENT = "enter_agent";
     /**
      * 微信小店 订单付款通知.
      */
