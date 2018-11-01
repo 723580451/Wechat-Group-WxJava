ef2d5f30fdd9f5f111d14d47c3f00cbf032b5d4a
Binary Wang <binarywang@gmail.com>
2018-11-01 20:44:39 +0800

    #821 查询企业付款结果接口的结果类增加payment_time属性

--- weixin-java-pay/src/main/java/com/github/binarywang/wxpay/bean/entpay/EntPayQueryResult.java
+++ weixin-java-pay/src/main/java/com/github/binarywang/wxpay/bean/entpay/EntPayQueryResult.java
@@ -22,55 +22,61 @@ public class EntPayQueryResult extends BaseWxPayResult {
   private static final long serialVersionUID = 3948485732447456947L;
 
   /**
-   * 商户订单号
+   * 商户订单号.
    */
   @XStreamAlias("partner_trade_no")
   private String partnerTradeNo;
 
   /**
-   * 付款单号
+   * 付款单号.
    */
   @XStreamAlias("detail_id")
   private String detailId;
 
   /**
-   * 转账状态
+   * 转账状态.
    */
   @XStreamAlias("status")
   private String status;
 
   /**
-   * 失败原因
+   * 失败原因.
    */
   @XStreamAlias("reason")
   private String reason;
 
   /**
-   * 收款用户openid
+   * 收款用户openid.
    */
   @XStreamAlias("openid")
   private String openid;
 
   /**
-   * 收款用户姓名
+   * 收款用户姓名.
    */
   @XStreamAlias("transfer_name")
   private String transferName;
 
   /**
-   * 付款金额
+   * 付款金额.
    */
   @XStreamAlias("payment_amount")
   private Integer paymentAmount;
 
   /**
-   * 转账时间
+   * 发起转账的时间.
    */
   @XStreamAlias("transfer_time")
   private String transferTime;
 
   /**
-   * 付款描述
+   * 企业付款成功时间.
+   */
+  @XStreamAlias("payment_time")
+  private String paymentTime;
+
+  /**
+   * 付款描述.
    */
   @XStreamAlias("desc")
   private String desc;
