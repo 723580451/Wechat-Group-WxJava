f45c8103e96a28b790b1c7eaa523f3bba9e485c6
Binary Wang <binarywang@gmail.com>
2019-01-20 13:49:30 +0800

    增加文本卡片消息的发送测试

--- weixin-java-cp/src/test/java/me/chanjar/weixin/cp/api/WxCpMessageAPITest.java
+++ weixin-java-cp/src/test/java/me/chanjar/weixin/cp/api/WxCpMessageAPITest.java
@@ -88,4 +88,23 @@ public class WxCpMessageAPITest {
     System.out.println(messageSendResult.getInvalidUserList());
     System.out.println(messageSendResult.getInvalidTagList());
   }
+
+  @Test
+  public void testSendMessage_textCard() throws WxErrorException {
+    WxCpMessage message = WxCpMessage
+      .TEXTCARD()
+      .toUser(configStorage.getUserId())
+      .btnTxt("更多")
+      .description( "<div class=\"gray\">2016年9月26日</div> <div class=\"normal\">恭喜你抽中iPhone 7一台，领奖码：xxxx</div><div class=\"highlight\">请于2016年10月10日前联系行政同事领取</div>")
+      .url("URL")
+      .title("领奖通知")
+      .build();
+
+    WxCpMessageSendResult messageSendResult = this.wxService.messageSend(message);
+    assertNotNull(messageSendResult);
+    System.out.println(messageSendResult);
+    System.out.println(messageSendResult.getInvalidPartyList());
+    System.out.println(messageSendResult.getInvalidUserList());
+    System.out.println(messageSendResult.getInvalidTagList());
+  }
 }
