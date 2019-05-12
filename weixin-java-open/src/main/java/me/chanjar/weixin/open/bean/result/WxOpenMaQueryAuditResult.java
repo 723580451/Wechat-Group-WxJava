3f386d0a56ee95d32ffd3ba7e189906408e61ddd
Binary Wang <binarywang@gmail.com>
2019-05-12 20:59:30 +0800

    #1011 开放平台小程序审核结果增加截图示例的字段

--- weixin-java-open/src/main/java/me/chanjar/weixin/open/bean/result/WxOpenMaQueryAuditResult.java
+++ weixin-java-open/src/main/java/me/chanjar/weixin/open/bean/result/WxOpenMaQueryAuditResult.java
@@ -29,4 +29,9 @@ public class WxOpenMaQueryAuditResult extends WxOpenResult {
    * 审核失败原因.
    */
   String reason;
+  /**
+   * 当status=1，审核被拒绝时，会返回审核失败的小程序截图示例。 xxx丨yyy丨zzz是media_id可通过获取永久素材接口 拉取截图内容）.
+   */
+  @SerializedName(value = "screenshot")
+  private String screenShot;
 }
